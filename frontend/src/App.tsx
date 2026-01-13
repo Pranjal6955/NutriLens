37e8eeabc8fa34f50cae87e93b9413a3667043db
Pranjal6955 <negipranjal555@gmail.com>
2026-01-13 22:33:38 +0530

    Added Prettier and lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
-    return (
-        <div className=" text-red-500 flex justify-center items-center h-screen">
-            <h1>App</h1>
-        </div>
-    );
+  return (
+    <div className=' text-red-500 flex justify-center items-center h-screen'>
+      <h1>App</h1>
+    </div>
+  );
 };
 
 export default App;
